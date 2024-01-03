ad4f0d1e205c78caf213ec0a6c0d03d72e142a41
simonecnascimento <scarneironascimento@gmail.com>
2024-01-03 10:13:50 -0500

    added IP InputParser

--- GetSegParams.m
+++ GetSegParams.m
@@ -1,5 +1,9 @@
 function segParams = GetSegParams(sbxInfo)
 
+IP = inputParser;
+addRequired( IP, 'sbxInfo', @isstruct )
+parse( IP, sbxInfo);
+
 paramsPath = sprintf('%s%s_seg_params.mat', sbxInfo.dir, sbxInfo.exptName ); 
 if exist(paramsPath, 'file')
     fprintf('\nLoading %s', paramsPath);
